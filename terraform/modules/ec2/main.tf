9d38ff0633a179e66ed973b8ff69580dede35894
mk8scloud-aws <mk8scloud@gmail.com>
2025-06-19 16:17:45 -0700

    added modules outputs..

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -6,9 +6,9 @@ resource "aws_key_pair" "mk_aws_key" {
 resource "aws_instance" "vm" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
-  subnet_id                   = aws_subnet.public.id
-  vpc_security_group_ids      = [aws_security_group.default.id]
-  key_name                    = aws_key_pair.laptop_key.key_name
+  subnet_id                   = module.vpc.public_subnet_id
+  vpc_security_group_ids      = [module.vpc.security_group_id]
+  key_name                    = aws_key_pair.mk_aws_key.key_name
   associate_public_ip_address = true
 
   tags = {
