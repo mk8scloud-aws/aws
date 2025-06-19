110b6bc3b1ae049b0561bc253f7ce9484753d599
mk8scloud-aws <123706761+mk8scloud-aws@users.noreply.github.com>
2025-06-19 14:54:06 -0700

    Create variables.tf

---
+++ terraform/modules/ec2/variables.tf
@@ -0,0 +1,13 @@
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "Ubuntu AMI ID for your region"
+  default     = "ami-05f991c49d264708f"  # Example: Ubuntu 22.04 in us-west-2, Free Tier
+}
+
+variable "public_key_path" {
+  description = "Path to your SSH public key file"
+  default     = "~/.ssh/id_rsa.pub"
+}
