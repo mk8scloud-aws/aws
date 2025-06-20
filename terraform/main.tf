6c2ab08622a690a68bc65e6d67c4b1709c613f49
mk8scloud-aws <mk8scloud@gmail.com>
2025-06-19 19:23:37 -0700

    fixed vpc_name.

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ module "vpc" {
   vpc_cidr            = "10.0.0.0/16"
   public_subnet_cidr  = "10.0.1.0/24"
   private_subnet_cidr = "10.0.2.0/24"
-  vpc_name            = "my-vpc"
+  vpc_name            = "mk-aws-vpc"
   availability_zone   = "us-west-2a"
 }
 
@@ -12,7 +12,7 @@ module "ec2" {
   ami_id          = "ami-05f991c49d264708f"
   instance_type   = "t2.micro"
   public_key_path = "~/.ssh/id_rsa.pub"
-  vpc_name        = "my-vpc"
+  vpc_name        = "mk-aws-vpc"
   public_subnet_id  = module.vpc.public_subnet_id
   security_group_id = module.vpc.security_group_id
 }
